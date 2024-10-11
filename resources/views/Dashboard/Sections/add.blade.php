9d650b5be2569a75b81ad8f04bb33b40328bd0be
AbdullahAnwarMohammed <abdullahanwarmohammed97@gmail.com>
2024-10-12 01:47:15 +0300

    first commit

---
+++ resources/views/Dashboard/Sections/add.blade.php
@@ -0,0 +1,34 @@
+<!-- Modal -->
+<div class="modal fade" id="add" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">{{trans('Dashboard/sections_trans.add_sections')}}</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form action="{{ route('Sections.store') }}" method="post" autocomplete="off">
+                @csrf
+                <div class="modal-body">
+                   
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">{{trans('Dashboard/sections_trans.name_sections')}}</label>
+                        <input type="text" name="name" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">{{__('Dashboard/sections_trans.description')}}</label>
+                        <textarea name="description" class="form-control" cols="30" rows="10"></textarea>
+                    </div>
+
+
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">{{trans('Dashboard/sections_trans.Close')}}</button>
+                    <button type="submit" class="btn btn-primary">{{trans('Dashboard/sections_trans.submit')}}</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
